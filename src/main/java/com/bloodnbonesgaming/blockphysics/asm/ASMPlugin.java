0d576eee36b00feefcae8ac92e4a933777bfd945
Blargerist <yaywarcraft@gmail.com>
2015-04-22 15:10:52 -0600

    Fix dependency issue.

--- src/main/java/com/bloodnbonesgaming/blockphysics/asm/ASMPlugin.java
+++ src/main/java/com/bloodnbonesgaming/blockphysics/asm/ASMPlugin.java
@@ -3,17 +3,17 @@ package com.bloodnbonesgaming.blockphysics.asm;
 import java.util.Map;
 
 import squeek.asmhelper.com.bloodnbonesgaming.lib.ObfHelper;
-
-import com.bnbgaming.lib.core.IBNBGamingPlugin;
-
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.DependsOn;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.SortingIndex;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
 
+@DependsOn("BNBGamingLibCore")
 @MCVersion("1.7.10")
 @SortingIndex(1001)
 @TransformerExclusions("com.bloodnbonesgaming.blockphysics.asm")
-public class ASMPlugin implements IBNBGamingPlugin
+public class ASMPlugin implements IFMLLoadingPlugin
 {
 	@Override
 	public String[] getASMTransformerClass()
